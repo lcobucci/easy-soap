30fa9860e6af33e48b1d1123b9300b4f730b1103
Luís Otávio Cobucci Oblonczyk <lcobucci@gmail.com>
2011-10-10 10:36:15 -0300

    __toString() added

--- src/Lcobucci/EasySoap/Annotations/WebServiceMethodAnnotation.php
+++ src/Lcobucci/EasySoap/Annotations/WebServiceMethodAnnotation.php
@@ -37,4 +37,70 @@ class WebServiceMethodAnnotation extends Annotation
 	{
 		$this->params = array();
 	}
+
+	/**
+	 * @return string
+	 */
+	public function __toString()
+	{
+		$doc = '@WebServiceMethod';
+		$options = array();
+
+		$this->addDescription($options);
+		$this->addParams($options);
+		$this->addParamName($options);
+		$this->addReturn($options);
+
+		if (isset($options[0])) {
+			$doc .= '(' . implode(', ', $options) . ')';
+		}
+
+		return $doc;
+	}
+
+	/**
+	 * @param array $options
+	 */
+	protected function addDescription(array &$options)
+	{
+		if (!is_null($this->description)) {
+			$options[] = "'description' => '" . $this->description . "'";
+		}
+	}
+
+	/**
+	 * @param array $options
+	 */
+	protected function addParamName(array &$options)
+	{
+		if (!is_null($this->responseParamName)) {
+			$options[] = "'responseParamName' => '" . $this->responseParamName . "'";
+		}
+	}
+
+	/**
+	 * @param array $options
+	 */
+	protected function addReturn(array &$options)
+	{
+		if (!is_null($this->return)) {
+			$options[] = "'return' => '" . $this->return . "'";
+		}
+	}
+
+	/**
+	 * @param array $options
+	 */
+	protected function addParams(array &$options)
+	{
+		if (isset($this->params[0])) {
+			$params = array();
+
+			foreach ($this->params as $param => $type) {
+				$params[] = "'" . $param . "' => '" . $type . "'";
+			}
+
+			$options[] = "'params' => array(" . implode(', ', $params) . ")";
+		}
+	}
 }
